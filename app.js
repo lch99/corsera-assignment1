a4e582f12b8d765b0421d4e658372c31adda83ea
CH <leecheehoe0620@gmail.com>
2021-07-07 17:03:36 +0800

    Solved

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope){
   $scope.click = function(){
+  $scope.borderStyle = {};
+  $scope.descriptionStyle = {};
 
     if(!$scope.value){
-        $scope.description = "Please enter data first."
-    }else if($scope.value){
+        $scope.display = function (){
+          $scope.borderStyle.style = {"border-color":"red"};
+          $scope.descriptionStyle.style = {"color":"red"};
+          return $scope.description = "Please enter data first.";
+        }
+    }else if ($scope.value) {
       $scope.display = function (){
 
         //capture the whole string with comma separator
@@ -38,13 +44,21 @@ function LunchCheckController($scope){
 
         //Display the message to user
         if(index <= 3 && index >= 1){
+          $scope.borderStyle.style = {"border-color":"green"};
+          $scope.descriptionStyle.style = {"color":"green"};
           return $scope.description = "Enjoy!"
         }else if(index > 3){
+            $scope.borderStyle.style = {"border-color":"green"};
+          $scope.descriptionStyle.style = {"color":"green"};
           return $scope.description = "Too much!"
         }else if (index == 0) {
+            $scope.borderStyle.style = {"border-color":"red"};
+          $scope.descriptionStyle.style = {"color":"red"};
           return $scope.description = "Please enter data first."
         }
       }
     }
-}}
+
+    }
+}
 })();
